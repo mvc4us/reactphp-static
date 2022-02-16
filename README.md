9750bcab3fca3b1321be4ebbd7da2c7726c03c34
Erdem Uncuoğlu <erdem@mvc4.us>
2022-02-16 20:27:15 +0300

    Namespace change. Constructor parameters.

--- README.md
+++ README.md
@@ -4,20 +4,30 @@ A simple middleware for serving static files with ReactPHP
 
 ## Installation
 
-```
-composer require ordinaryjellyfish/react-static
+```shell
+composer require mvc4us/react-static
 ```
 
 ## Usage
 
-Use it in your ReactPHP like a normal middleware, passing in your webroot:
-
+Use it in your ReactPHP like a normal middleware, passing in an array of base url(s) pointing to associated webroot(s).
+All the files under any webroot (including subdirectories) will be served.
 ```php
-new OrdinaryJellyfish\ReactStatic\StaticServer(__DIR__);
+new Mvc4us\ReactStatic\StaticServer([
+    "/" => "/wwwroot/domain/public",
+    "/docs" => "/documents/from/other/directory"
+]);
 ```
 
-The middleware will serve any static files if they exist. If a file does not exist for the requested path, the middleware will exit, letting you run the rest of your application.
+Optionally to exclude some files you can define an array of shell patterns as a second parameter:
+
+```php
+new Mvc4us\ReactStatic\StaticServer(
+    ["/" => "/webroot"],
+    ["*.php", ".htaccess", "*private*"]
+);
+```
+
+The middleware will serve any static files if they exist or return 404 response if it is excluded. If a file does not exist for the requested path, it continues processing next middleware, letting you run the rest of your application.
 
 Enjoy!
-
-[Buy me a coffee <3](https://paypal.me/tristiank3604)
--- src/StaticServer.php
+++ src/StaticServer.php
@@ -1,29 +1,48 @@
 <?php
+declare(strict_types=1);
 
-namespace OrdinaryJellyfish\ReactStatic;
+namespace Mvc4us\ReactStatic;
 
 use Psr\Http\Message\ServerRequestInterface;
-use React\Http\Response;
+use React\Http\Message\Response;
 
 class StaticServer
 {
-    /**
-     * @var string
-     */
-    private $path;
 
-    public function __construct(string $path)
+    private array $path;
+
+    private array $exclude;
+
+    /**
+     * @param array $path Associated array of url base paths as keys and absolute directory paths as values to serve static files from.
+     * @param array $exclude Array of shell patterns for file exclusion.
+     */
+    public function __construct(array $path, array $exclude = [])
     {
         $this->path = $path;
+        $this->exclude = $exclude;
     }
 
-    public function __invoke()
+    public function __invoke(ServerRequestInterface $request, callable $next): Response
     {
-        return function (ServerRequestInterface $request, $next) {
-            $filePath = $request->getUri()->getPath();
-            $file = $this->path.$filePath;
+        if (empty($this->path)) {
+            return $next($request);
+        }
+
+        foreach ($this->path as $urlBase => $path) {
+            $urlPath = $request->getUri()->getPath();
+            if ($urlBase !== '/') {
+                if (!str_starts_with($urlPath, $urlBase)) {
+                    continue;
+                }
+                $urlPath = str_replace($urlBase, '', $urlPath);
+            }
+            $file = $path . $urlPath;
 
             if (file_exists($file) && !is_dir($file)) {
+                if ($this->isExcluded($file)) {
+                    return new Response(404);
+                }
                 $fileExt = pathinfo($file, PATHINFO_EXTENSION);
                 $finfo = finfo_open(FILEINFO_MIME_TYPE);
                 $fileType = finfo_file($finfo, $file);
@@ -35,17 +54,27 @@ class StaticServer
                     case 'css':
                         $fileType = 'text/css';
 
-                    break;
+                        break;
                     case 'js':
                         $fileType = 'application/javascript';
 
-                    break;
+                        break;
                 }
 
                 return new Response(200, ['Content-Type' => $fileType], $fileContents);
             }
+        }
 
-            return $next($request);
-        };
+        return $next($request);
+    }
+
+    private function isExcluded($file): bool
+    {
+        foreach ($this->exclude as $pattern) {
+            if (fnmatch($pattern, basename($file))) {
+                return true;
+            }
+        }
+        return false;
     }
 }
